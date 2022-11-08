262c8e8e08c12244514a04f0dbc1d4cdf17e720f
mkaychuks <sachufusi@gmail.com>
2022-11-08 12:18:07 +0100

    created mmy contacts model

---
+++ lib/models/contacts.dart
@@ -0,0 +1,8 @@
+class ContactsModel {
+  final int id;
+  final String name;
+  final String address;
+  final String bio;
+
+  ContactsModel({required this.name, required this.address, required this.bio, required this.id});
+}
