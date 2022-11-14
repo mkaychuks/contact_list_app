6ca7dec25d019dac5da8ee35f6bf3c3e1df7c192
mkaychuks <sachufusi@gmail.com>
2022-11-14 17:51:06 +0100

    reuseable widget for form fields

---
+++ lib/widgets/textfield_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  TextFieldWidget(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText,
+      required this.textFieldIcon});
+
+  final bool obscureText;
+  final Widget? textFieldIcon;
+  final String hintText;
+  final TextEditingController controller;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration:  InputDecoration(
+        prefixIcon: textFieldIcon,
+        hintText: hintText,
+      ),
+      maxLength: 20,
+      obscureText: obscureText,
+    );
+  }
+}
